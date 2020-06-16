b41a6563a4eacc8a1ac045e9482eda6817062ecc
baha eddine <ayadibaha51@gmail.com>
2020-06-16 19:47:54 +0100

    fixing

--- add_emplacement.php
+++ add_emplacement.php
@@ -1,10 +1,10 @@
 <?php
 include("./config/db.php");
-if (sizeof($_GET) > 0) {
-    $lat = $_GET['lat'];
-    $lng = $_GET['lng'];
-    $client = $_GET['client'];
-    $query = "INSERT INTO emplacement(latitude, longitude, infected_cin) VALUES('" . $lat . "', '" . $lng . "','12345678')";
-    $result = $db->query($query);
-    echo $result;
-}
+// if (sizeof($_GET) > 0) {
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
+// $client = $_GET['client'];
+$query = "INSERT INTO emplacement(latitude, longitude, infected_cin) VALUES('" . $lat . "', '" . $lng . "','12345678')";
+$result = mysqli_query($db, $query);
+echo $result;
+// }
