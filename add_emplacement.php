7daacfe436754580240a50c2f9a41e375e658b3b
baha eddine <ayadibaha51@gmail.com>
2020-06-16 19:46:18 +0100

    fix

--- add_emplacement.php
+++ add_emplacement.php
@@ -4,7 +4,7 @@ if (sizeof($_GET) > 0) {
     $lat = $_GET['lat'];
     $lng = $_GET['lng'];
     $client = $_GET['client'];
-    $query = "INSERT INTO emplacement(latitude, longitude, infected_id VALUES('" . $lat . "', '" . $lng . "','12345678')";
+    $query = "INSERT INTO emplacement(latitude, longitude, infected_cin) VALUES('" . $lat . "', '" . $lng . "','12345678')";
     $result = $db->query($query);
     echo $result;
 }
