cdf1b247af7b1d753f454e573f5506b5a5d9d9e2
baha eddine <ayadibaha51@gmail.com>
2020-06-16 08:15:31 +0100

    update database

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "projet";
+$servername = "sql7.freesqldatabase.com";
+$username = "sql7348763";
+$password = "qlJstCriwr";
+$database = "sql7348763";
 // Create database connection MySQL
 $db = mysqli_connect($servername, $username, $password, $database);
