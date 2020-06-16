c3d91f0443c5b2a380c320528a87226f2f84339d
baha eddine <ayadibaha51@gmail.com>
2020-06-16 08:25:19 +0100

    fix logo

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,7 +1,7 @@
-<div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="/template/theme-assets/images/backgrounds/02.jpg">
+<div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true" data-img="/template/theme-assets/images/backgrounds/02.jpg">
   <div class="navbar-header">
     <ul class="nav navbar-nav flex-row">
-      <li class="nav-item mr-auto"><a class="navbar-brand" href="index.php"><img class="brand-logo" style="width: 130px;margin-top: -15px;margin-left: 35%;" alt="Covid-19" src="/projet/login/images/covid19tunisia-fb.png" />
+      <li class="nav-item mr-auto"><a class="navbar-brand" href="index.php"><img class="brand-logo" style="width: 130px;margin-top: -15px;margin-left: 35%;" alt="Covid-19" src="/login/images/covid19tunisia-fb.png" />
           <h3 class="brand-text"></h3>
         </a></li>
       <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
